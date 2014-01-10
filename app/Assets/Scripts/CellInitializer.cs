4e84ae7c4d6a064a11ab119244d364291ca58e20
Mike Judge <mikelovesrobots@gmail.com>
2014-01-10 15:59:46 -0800

    Add cell animations

---
+++ app/Assets/Scripts/CellInitializer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellInitializer : MonoBehaviour {
+    public Animator Animator;
+    void Start () {
+        StartCoroutine(WaitThenAnimate());
+    }
+	
+    private IEnumerator WaitThenAnimate() {
+        yield return new WaitForSeconds(RandomWait);
+        Animator.enabled = true;
+    }
+
+    private float RandomWait {
+        get { return Random.Range(0, 0.5f); }
+    }
+}
