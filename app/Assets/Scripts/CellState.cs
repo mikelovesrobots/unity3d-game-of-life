fff5c994cb96d4a7701fb38d9586f8a49f4ac150
Mike Judge <mikelovesrobots@gmail.com>
2014-01-10 17:19:42 -0800

    On mouseover enable cells

--- app/Assets/Scripts/CellState.cs
+++ app/Assets/Scripts/CellState.cs
@@ -2,15 +2,20 @@
 using System.Collections;
 
 public class CellState : MonoBehaviour {
-    private const float ANIMATION_TIME = 1f;
+    private const float DEFAULT_ANIMATION_TIME = 1f;
     public GameObject Sprite;
     public bool IsAlive;
 
     public void Instantiate(bool isAlive) {
-        transform.localScale = DestinationScale(isAlive);
-        IsAlive = isAlive;
+        InstantTransition(isAlive);
     }
 
+    public void InstantTransition(bool isAlive) {
+        iTween.Stop(Sprite);
+        Sprite.transform.localScale = DestinationScale(isAlive);
+        IsAlive = isAlive;
+    }
+    
     public void Transition(bool isAlive) {
         if (isAlive != IsAlive) {
             ScaleTo(DestinationScale(isAlive));
@@ -21,7 +26,7 @@ public class CellState : MonoBehaviour {
     private void ScaleTo(Vector3 scale) {
         var options = new Hashtable();
         options.Add("scale", scale);
-        options.Add("time", ANIMATION_TIME);
+        options.Add("time", DEFAULT_ANIMATION_TIME);
         options.Add("easetype", "easeinoutquad");
         iTween.ScaleTo(Sprite, options);
     }
---
+++ app/Assets/Scripts/OnMouseOverEnableCell.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnMouseOverEnableCell : MonoBehaviour {
+    public CellState CellState;
+
+    public void OnMouseOver() {
+        CellState.InstantTransition(true);
+    }
+}
